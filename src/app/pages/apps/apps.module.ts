c4a018f5745737220d57c24a73752fa260eb69cb
Jay-Khodiyar <jaykhodiyarcreation93@gmail.com>
2026-05-21 15:06:50 +0530

    file change fix

--- src/app/pages/apps/apps.module.ts
+++ src/app/pages/apps/apps.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { CommonModule, DatePipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../../material.module';
 import { NgxPermissionsModule } from 'ngx-permissions';
@@ -16,8 +16,6 @@ import { NgScrollbarModule } from 'ngx-scrollbar';
 import { TablerIconsModule } from 'angular-tabler-icons';
 import * as TablerIcons from 'angular-tabler-icons/icons';
 
-//Chat
-import { AppChatComponent } from './chat/chat.component';
 //Contact
 import { AppContactDialogContentComponent } from './contact/contact.component';
 import { AppContactComponent } from './contact/contact.component';
@@ -25,55 +23,20 @@ import { AppContactComponent } from './contact/contact.component';
 import { AppCoursesComponent } from './courses/courses.component';
 import { AppCourseDetailComponent } from './courses/course-detail/course-detail.component';
 
-//Notes
-import { AppNotesComponent } from './notes/notes.component';
-//Todo
-import { AppTodoComponent } from './todo/todo.component';
 // Permission
 import { AppPermissionComponent } from './permission/permission.component';
-//Mailbox
-import {
-  ListingComponent,
-  ListingDialogDataExampleDialogComponent,
-} from './email/listing/listing.component';
-import { DetailComponent } from './email/detail/detail.component';
-import { AppEmailComponent } from './email/email.component';
-
-//Taskboard
-import { AppTaskboardComponent } from './taskboard/taskboard.component';
-import { TaskDialogComponent } from './taskboard/task-dialog.component';
-import { OkAppTaskComponent } from './taskboard/ok-task/ok-task.component';
-import { DeleteAppTaskComponent } from './taskboard/delete-task/delete-task.component';
 
 //Calendar
-import { AppFullcalendarComponent } from './fullcalendar/fullcalendar.component';
-import { CalendarDialogComponent } from './fullcalendar/fullcalendar.component';
-import { CalendarFormDialogComponent } from './fullcalendar/calendar-form-dialog/calendar-form-dialog.component';
+
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
-import { AppEmployeeComponent } from './employee/employee.component';
-import { AppEmployeeDialogContentComponent } from './employee/employee.component';
-import { AppAddEmployeeComponent } from './employee/add/add.component';
 
 import { AppsRoutes } from './apps.routing';
 import { MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
-import {
-  AppTicketlistComponent,
-  AppTicketDialogContentComponent,
-} from './ticketlist/ticketlist.component';
 
-//Invoice
-import { AppInvoiceListComponent } from './invoice/invoice-list/invoice-list.component';
-import { AppInvoiceViewComponent } from './invoice/invoice-view/invoice-view.component';
-import { AppAddInvoiceComponent } from './invoice/add-invoice/add-invoice.component';
-import { AppEditInvoiceComponent } from './invoice/edit-invoice/edit-invoice.component';
-import { OkDialogComponent } from './invoice/edit-invoice/ok-dialog/ok-dialog.component';
-import { AddedDialogComponent } from './invoice/add-invoice/added-dialog/added-dialog.component';
 
 // blog
-import { AppBlogsComponent } from './blogs/blogs.component';
-import { AppBlogDetailsComponent } from './blogs/details/details.component';
 import { MedicalComponent } from './medical/medical.component';
 import { AddmedicaldialogComponent } from './medical/addmedicaldialog/addmedicaldialog.component';
 import { LaboratoryComponent } from './laboratory/laboratory.component';
@@ -131,38 +94,11 @@ import { MedicineViewComponent } from './medicine/medicine-view/medicine-view.co
   ],
   exports: [TablerIconsModule,MedicineComponent,MedicalComponent],
   declarations: [
-    AppChatComponent,
     AppPermissionComponent,
-    AppNotesComponent,
-    AppTodoComponent,
-    AppTaskboardComponent,
-    TaskDialogComponent,
-    OkAppTaskComponent,
-    DeleteAppTaskComponent,
-    ListingDialogDataExampleDialogComponent,
-    ListingComponent,
-    DetailComponent,
-    AppEmailComponent,
-    AppFullcalendarComponent,
-    CalendarDialogComponent,
-    CalendarFormDialogComponent,
-    AppTicketlistComponent,
-    AppTicketDialogContentComponent,
     AppContactComponent,
     AppContactDialogContentComponent,
     AppCoursesComponent,
     AppCourseDetailComponent,
-    AppEmployeeComponent,
-    AppEmployeeDialogContentComponent,
-    AppAddEmployeeComponent,
-    AppInvoiceListComponent,
-    AppInvoiceViewComponent,
-    AppAddInvoiceComponent,
-    AppEditInvoiceComponent,
-    AddedDialogComponent,
-    OkDialogComponent,
-    AppBlogsComponent,
-    AppBlogDetailsComponent,
     MedicalComponent,
     AddmedicaldialogComponent,
     LaboratoryComponent,
